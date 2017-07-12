233e4862031e1554e684b6653f743818839e86c7
Artur Gawlik <arturgawlik@MacBook-Air-Artur.local>
2017-07-12 13:34:16 +0200

    adding base application structure

---
+++ src/theMist.Infrastructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace theMist.Infrastructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
